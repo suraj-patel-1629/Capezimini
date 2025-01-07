be4ef948d484941718e78cf470d830dce55727f7
suraj patel <sp9589890205@gmail.com>
2025-01-07 10:13:13 +0530

    Added new-file.txt

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Calculator{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+System.out.println("Calculator");
+System.out.println("Enter the 1 digit");
+
+int a = sc.nextInt();
+System.out.println("Enter the opertor you need to perform");
+char op= sc.next().charAt(0);
+System.out.println("Enter the 2 digit");
+int b = sc.nextInt();
+
+if(op=='+'){
+  System.out.println(a+b);
+}
+else if(op=='-'){
+  System.out.println(a-b);
+}else if(op=='*'){
+  System.out.println(a*b);
+}
+else if(op=='/'){
+  System.out.println(a/b);
+}
+else{
+System.out.println("Operator does not exist");
+}
+
+
+
+}
+}
